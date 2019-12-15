37aa5993cf243cb0f3b5e9bc5687ef783edd3d12
Joshua Jung <tug76804@temple.edu>
2019-12-15 16:04:17 -0500

    first commit

---
+++ validation.js
@@ -0,0 +1,10 @@
+var pass="Your password";
+var password;
+password = prompt("Enter Password to Upload Comments! (Hint: It's 12345)", ' ');
+
+if(password==pass){
+    alert('Correct Password, Click Ok to Entet Comments Page')
+}
+else{
+    alert('That is not the correct password!')
+}
